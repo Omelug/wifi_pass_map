fc4e346cbfec149aebce786291b684e1e7fdf2f1
Vit Janecek <xjanecv00@vutbr.cz>
2025-03-23 23:59:01 +0100

    move tool render login to templete

--- map_app/web/static.py
+++ map_app/web/static.py
@@ -18,29 +18,8 @@ def wifi_pass_map():
 
 @static_bp.route('/tools')
 def tools():
-    buttons_html = ""
-    for script_name, tool_info in tool_management.tool_list().items():
-        script_name = os.path.splitext(script_name)[0]
-        for tool_name, tool_details in tool_info.items():
-            buttons_html += f'''
-                <div class="tool-container">
-                    <button id="{script_name}_{tool_name}_button" onclick="runTool('{script_name}', '{tool_name}')">{tool_name.upper()}</button>
-                    <button id="{script_name}_{tool_name}_save_button" class="save-button" onclick="saveParams('{script_name}', '{tool_name}')">S</button>
-                    <div id="{script_name}_{tool_name}_result" class="result"></div>
-                </div>
-            '''
-
-            if "params" in tool_details:
-                for param_name, param_type, param_control, param_old_value, param_desc in tool_details["params"]:
-                    buttons_html += f'''
-                      <div class="input-container">
-                          <label for="{script_name}_{tool_name}_{param_name}"> {param_type.__name__} | {param_name}</label>
-                          <input type="text" id="{script_name}_{tool_name}_{param_name}" name="{param_name}" value="{param_old_value}">
-                          <label>{param_desc}</label>
-                      </div>
-                      '''
-
-    return render_template('tools.html', buttons_html=buttons_html)
+    tool_data = tool_management.tool_list()
+    return render_template('tools.html', tools=tool_data)
 
 @static_bp.route('/images/<path:filename>')
 def serve_image(filename):
