932244c483ae3c6234390d4903317a247bb2c9a2
Vit Janecek <xjanecv00@vutbr.cz>
2025-03-24 11:08:15 +0100

    api.py refactoring

--- map_app/endpoints/api.py
+++ map_app/endpoints/api.py
@@ -2,73 +2,62 @@ import configparser
 import logging
 import os
 from flask import jsonify, request, Response, stream_with_context, Blueprint
+from formator.files import source_script_name
 from map_app import sources
 
 api_bp = Blueprint('api', __name__)
+
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
-POT_UPLOAD_FOLDER = 'app/data/potfile'
-ALLOWED_EXTENSIONS = {'pot', '22000', 'potfile'}
+SAFE_CONFIG_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "config"))
+
 
 # ------------MAPS--------------
 @api_bp.route('/api/wifi_pass_map')
 def pwnapi():
     """Load first data to map"""
     log.debug(f"Request Path: {request.path} was called")
-
     pwned_data,script_statuses = sources.get_AP_data()
-    return jsonify({
-        'data': pwned_data,
-        'script_statuses': script_statuses,
-        'AP_len': len(pwned_data)
-    })
+    return jsonify({'data': pwned_data,'script_statuses': script_statuses,'AP_len': len(pwned_data)})
 
 @api_bp.route('/api/explore')
 def exploreapi():
+    """Load filtered AP data"""
     log.debug(f"Request Path: {request.path} was called")
 
-    # Extract filters from request parameters, if not None
-    filters = {key: value for key, value in {
+    filters = {k:v for k,v in {
         'essid': request.args.get('name'),
         'bssid': request.args.get('network_id'),
         'limit': request.args.get('limit'),
         'encryption': request.args.get('encryption'),
         'network_type': request.args.get('network_type'),
-        #'exclude_no_ssid': request.args.get('exclude_no_ssid', 'false')
-    }.items() if value is not None}
+    }.items() if v is not None}
 
-    pwned_data, script_statuses = sources.get_AP_data(filters=filters)
-    return jsonify({
-        'data': pwned_data,
-        'script_statuses': script_statuses,
-        'AP_len': len(pwned_data)
-    })
+    ap_data, script_statuses = sources.get_AP_data(filters=filters)
+    return jsonify({'data': ap_data, 'script_statuses': script_statuses, 'AP_len': len(ap_data) })
 
 
-@api_bp.route('/api/load_sqare')
+@api_bp.route('/api/load_sqare', methods=["POST"])
 def load_sqare():
     log.debug(f"Request Path: {request.path} was called")
 
-    # Extract params from request parameters
-    filters = {key: value for key, value in {
-        'center_latitude': request.args.get('center_latitude'),
-        'center_longitude': request.args.get('center_longitude'),
-        'sqare_limit': request.args.get('sqare_limit'),
-    }.items() if value is not None}
+    filters = {k: v for k, v in {
+        "center_latitude": request.args.get("center_latitude"),
+        "center_longitude": request.args.get("center_longitude"),
+        "square_limit": request.args.get("square_limit"),
+    }.items() if v is not None}
 
-    pwned_data, script_statuses = sources.get_AP_data(filters)
-    return jsonify({
-        'data': pwned_data,
-        'script_statuses': script_statuses,
-        'AP_len': len(pwned_data)
-    })
+    ap_data, script_statuses = sources.get_AP_data(filters)
+    return jsonify({'data': ap_data,'script_statuses': script_statuses,'AP_len': len(ap_data)})
 
 
 # ------------TOOLS--------------
 @api_bp.route('/api/tools', methods=['POST'])
 def run_script():
+    """Run specified tool script, stream its output."""
     tools = sources.tool_list()
+
     # Get script name and optional arguments from the POST request
     script_name = request.json.get('script_name')
     tool_name = request.json.get('tool_name')
@@ -104,7 +93,8 @@ def run_script():
 
 @api_bp.route('/api/save_params', methods=['POST'])
 def save_params():
-    data = request.json
+    """Save user-defined parameters for a given source script and tool."""
+    data = request.json or {}
     script_name = data.get('script_name')
     tool_name = data.get('tool_name')
     params = data.get('params')
@@ -112,21 +102,23 @@ def save_params():
     if not script_name or not tool_name or not params:
         return {"status": "error", "message": "Script name or tool name or parameters missing"}, 400
 
-    config_file = os.path.join(os.path.dirname(__file__), '..', 'sources', f'config/{script_name}.ini')
-    config = configparser.ConfigParser()
+    #secure input
+    config_file = os.path.join(SAFE_CONFIG_DIR, f"{script_name}.ini")
+    if not config_file.startswith(SAFE_CONFIG_DIR) or not source_script_name(script_name):
+        return {"status": "error", "message": "Invalid script name."}, 400
 
+    config = configparser.ConfigParser()
     if os.path.exists(config_file):
         config.read(config_file)
     else:
         return {"status": "error", "message": f"Config file for {script_name} not found"}, 404
 
 
-    # Update the tool section with the new parameters
+    #TODO  Update the tool section with the new parameters
     if tool_name not in config:
         config[tool_name] = {}
 
     for param_name, param_value in params.items():
-        #TODO check param control function from tools list
         config[tool_name][param_name] = str(param_value)
 
     with open(config_file, 'w') as cf:
